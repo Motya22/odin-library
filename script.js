2cb54d1d42b45e40c093197ea671c90660bd49dc
Motya22 <motya2222@gmail.com>
2023-10-05 14:58:31 +0300

    Add the "Book" example from the previous lesson

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.read ? 'already read' : 'not read yet'
+  }`;
+};
